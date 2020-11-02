485280ec17d5cc87f9e878623abb9e656161c0cb
Ferran <ferrangil@gmail.com>
2020-11-02 11:07:25 +0100

    Update index.js
    quick edit from github

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: '1rem',
             }}
           >
-            Latest Stories
+            Latest Stories of the demo site
           </h1>
         </div>
         <section className="section">
